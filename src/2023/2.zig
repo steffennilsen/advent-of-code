55a07f20f681f9d171d96f3c42832546f7a18559
steffen nilsen <steffen.nilsen@gmail.com>
2023-12-06 18:41:24 +0100

    day2 part 1 tests

--- src/2023/2.zig
+++ src/2023/2.zig
@@ -70,3 +70,33 @@ fn parseGame(line: []const u8) !usize {
 
     return id;
 }
+
+test "p1_game_1" {
+    const line: []const u8 = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(1, actual);
+}
+
+test "p1_game_2" {
+    const line: []const u8 = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(2, actual);
+}
+
+test "p1_game_3" {
+    const line: []const u8 = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(0, actual);
+}
+
+test "p1_game_4" {
+    const line: []const u8 = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(0, actual);
+}
+
+test "p1_game_5" {
+    const line: []const u8 = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(5, actual);
+}
