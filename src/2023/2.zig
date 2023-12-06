9bdfc71cc0629383ec923739e1f574058b7e8a94
steffen nilsen <steffen.nilsen@gmail.com>
2023-12-06 18:55:33 +0100

    day2 part 2 tests

--- src/2023/2.zig
+++ src/2023/2.zig
@@ -109,3 +109,33 @@ test "p1_game_5" {
     const actual = comptime try parseGame(line);
     try std.testing.expectEqual(5, actual.p1);
 }
+
+test "p2_game_1" {
+    const line: []const u8 = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(48, actual.p2);
+}
+
+test "p2_game_2" {
+    const line: []const u8 = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(12, actual.p2);
+}
+
+test "p2_game_3" {
+    const line: []const u8 = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(1560, actual.p2);
+}
+
+test "p2_game_4" {
+    const line: []const u8 = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(630, actual.p2);
+}
+
+test "p2_game_5" {
+    const line: []const u8 = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    const actual = comptime try parseGame(line);
+    try std.testing.expectEqual(36, actual.p2);
+}
