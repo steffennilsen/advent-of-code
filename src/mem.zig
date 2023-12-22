582a649b24252e044fd1f8d89a6ed25c192a23d7
steffen nilsen <steffen.nilsen@gmail.com>
2023-12-22 10:02:14 +0100

    --wip-- [skip ci]

--- src/mem.zig
+++ src/mem.zig
@@ -4,12 +4,9 @@ const InnerList = std.ArrayList(usize);
 const OuterList = std.ArrayList(InnerList);
 const Map = std.AutoHashMap(usize, OuterList);
 
-fn external_fn(allocator: std.mem.Allocator, map: *Map) !void {
-    var ol_1 = OuterList.init(allocator);
-    try map.put(1, ol_1);
-
+fn external_fn(allocator: std.mem.Allocator, ol: OuterList) !void {
     var li_1_1 = InnerList.init(allocator);
-    try ol_1.append(li_1_1);
+    try ol.append(li_1_1);
     try li_1_1.append(11);
 }
 
@@ -20,12 +17,12 @@ test "map" {
 
     var map = Map.init(allocator);
     defer map.deinit();
-    try external_fn(allocator, &map);
 
+    var ol_1 = OuterList.init(allocator);
+    try map.put(1, ol_1);
     try std.testing.expectEqual(@as(usize, 1), map.count());
 
-    const ol_1: OuterList = map.get(1).?;
-    try std.testing.expectEqual(@as(usize, 1), ol_1.items.len);
+    try external_fn(allocator, &ol_1);
 
     const li_1_1: InnerList = ol_1.items[0];
     try std.testing.expectEqual(@as(usize, 1), li_1_1.items.len);
