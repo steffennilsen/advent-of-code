39fac1e71f080121c8d9cca2ce24ab9abe1ee1f9
steffen nilsen <steffen.nilsen@gmail.com>
2023-12-22 10:05:27 +0100

    --wip-- [skip ci]

--- src/mem.zig
+++ src/mem.zig
@@ -2,29 +2,23 @@ const std = @import("std");
 
 const InnerList = std.ArrayList(usize);
 const OuterList = std.ArrayList(InnerList);
-const Map = std.AutoHashMap(usize, OuterList);
 
-fn external_fn(allocator: std.mem.Allocator, ol: OuterList) !void {
+fn external_fn(allocator: std.mem.Allocator, ol: *OuterList) !void {
     var li_1_1 = InnerList.init(allocator);
     try ol.append(li_1_1);
     try li_1_1.append(11);
 }
 
-test "map" {
+test "list of lists" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var map = Map.init(allocator);
-    defer map.deinit();
+    var ol = OuterList.init(allocator);
+    try external_fn(allocator, &ol);
+    try std.testing.expectEqual(@as(usize, 1), ol.items.len);
 
-    var ol_1 = OuterList.init(allocator);
-    try map.put(1, ol_1);
-    try std.testing.expectEqual(@as(usize, 1), map.count());
-
-    try external_fn(allocator, &ol_1);
-
-    const li_1_1: InnerList = ol_1.items[0];
-    try std.testing.expectEqual(@as(usize, 1), li_1_1.items.len);
-    try std.testing.expectEqual(@as(usize, 11), li_1_1.items[0]);
+    const il: InnerList = ol.items[0];
+    try std.testing.expectEqual(@as(usize, 1), il.items.len);
+    try std.testing.expectEqual(@as(usize, 11), il.items[0]);
 }
