b1bcd3e1cae974083870829a02ea9f477bf6b6ea
steffen nilsen <steffen.nilsen@gmail.com>
2023-12-22 09:33:52 +0100

    --wip-- [skip ci]

--- src/mem.zig
+++ src/mem.zig
@@ -1,63 +1,33 @@
 const std = @import("std");
 
-const T = usize;
 const InnerList = std.ArrayList(usize);
-const OuterList = std.ArrayList(*InnerList);
+const OuterList = std.ArrayList(InnerList);
+const Map = std.AutoHashMap(usize, OuterList);
 
-fn list(allocator: std.mem.Allocator, outerList: *OuterList) !void {
-    std.debug.print("\n", .{});
-    std.debug.print("ol: t > {}\n", .{@TypeOf(outerList)});
-    std.debug.print("ol: t.*> {}\n", .{@TypeOf(outerList.*)});
+fn mapper(allocator: std.mem.Allocator, map: *Map) !void {
+    var ol_1 = OuterList.init(allocator);
+    try map.put(1, ol_1);
 
-    var innerList1 = InnerList.init(allocator);
-    try outerList.append(&innerList1);
-    try innerList1.append(4);
-    try innerList1.append(5);
-
-    std.debug.assert(innerList1.items.len == 2);
-    std.debug.assert(innerList1.items[0] == 4);
-    std.debug.assert(innerList1.items[1] == 5);
-
-    var innerList2: *InnerList = outerList.*.items[0];
-    try innerList2.append(7);
-    try innerList2.append(8);
-    try innerList2.append(9);
-
-    std.debug.print("debug> {any}\n", .{innerList2.items.len});
-    // std.debug.assert(innerList2.items.len == 3);
-    // std.debug.assert(innerList2.items[0] == 7);
-    // std.debug.assert(innerList2.items[1] == 8);
-    // std.debug.assert(innerList2.items[2] == 9);
-
-    std.debug.print("l1> {any}\n", .{innerList1.items});
-    std.debug.print("l2> {any}\n", .{innerList2.items});
-
-    std.debug.print("ol  > {any}\n", .{outerList.items.len});
-    std.debug.print("ol.*> {any}\n", .{outerList.*.items.len});
-
-    std.debug.print("ol  > {any}\n", .{outerList.items[0].items.len});
-    std.debug.print("ol.*> {any}\n", .{outerList.*.items[0].items.len});
+    var li_1_1 = InnerList.init(allocator);
+    try ol_1.append(li_1_1);
+    try li_1_1.append(11);
 }
 
-test "list" {
+test "map" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var outerList = OuterList.init(allocator);
-    defer outerList.deinit();
-    defer for (outerList.items) |item| item.deinit();
+    var map = Map.init(allocator);
+    defer map.deinit();
+    try mapper(allocator, &map);
 
-    try list(allocator, &outerList);
-    try std.testing.expectEqual(@as(usize, 1), outerList.items.len);
+    try std.testing.expectEqual(@as(usize, 1), map.count());
 
-    var innerList: *InnerList = outerList.items[0];
-    try std.testing.expectEqual(*InnerList, @TypeOf(innerList));
-    try std.testing.expectEqual(@as(usize, 5), innerList.items.len);
+    const ol_1: OuterList = map.get(1).?;
+    try std.testing.expectEqual(@as(usize, 1), ol_1.items.len);
 
-    try std.testing.expectEqual(@as(usize, 4), innerList.items[0]);
-    try std.testing.expectEqual(@as(usize, 5), innerList.items[1]);
-    try std.testing.expectEqual(@as(usize, 7), innerList.items[2]);
-    try std.testing.expectEqual(@as(usize, 8), innerList.items[3]);
-    try std.testing.expectEqual(@as(usize, 9), innerList.items[4]);
+    const li_1_1: InnerList = ol_1.items[0];
+    try std.testing.expectEqual(@as(usize, 1), li_1_1.items.len);
+    try std.testing.expectEqual(@as(usize, 11), li_1_1.items[0]);
 }
